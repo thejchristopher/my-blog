e5b108783cd5bff8f4e1bfcaacc2573d743b5296
Jason Muhammad <thejchristopher@gmail.com>
2021-11-14 10:02:48 -0600

    First commit

---
+++ src/build/precache-manifest.1c24403311d52a036a7ab179260bf836.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "884ff381bb60322a419a",
+    "url": "/static/css/main.93e18809.chunk.css"
+  },
+  {
+    "revision": "884ff381bb60322a419a",
+    "url": "/static/js/main.11afc59c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "62b65faaa5053cc8309e",
+    "url": "/static/js/2.7bd8fa86.chunk.js"
+  },
+  {
+    "revision": "1f453d412f674fa0f5583d0c3e3ba651",
+    "url": "/index.html"
+  }
+];
